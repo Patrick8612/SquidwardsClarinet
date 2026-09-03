d1af46900c58a4e944fd8d38c0443b5656b952fb
Patrick8612 <3259247640@qq.com>
2026-09-03 10:32:24 +0800

    5

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -1,5 +1,16 @@
+import socket
 target = input("请输入目标IP：")
 port = int(input("请输入端口："))
 
-print("目标是：", target)
-print("端口是：", port)
+s=socket.socket()
+print("开始连接")
+
+try:
+    s.connect((target,port))
+    print("端口开放")
+except:
+    print("端口关闭")
+finally:
+    s.close()
+
+
