eb0e07e5fef5bc284637ec6096e0c39993788f95
Patrick8612 <3259247640@qq.com>
2026-09-04 11:24:12 +0800

    7

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -3,7 +3,10 @@ import socket
 target = input("请输入目标IP：")
 has_open = False
 
-for port in range(1, 101):
+start_port=int(input("请输入起始端口："))
+end_port=int(input("请输入结束端口："))
+
+for port in range(start_port, end_port + 1):
 
     s = socket.socket()
     s.settimeout(0.5)
@@ -11,9 +14,10 @@ for port in range(1, 101):
     try:
         s.connect((target, port))
         print(f"[+] 端口 {port} 开放")
+        has_open = True
     except:
         pass
 
     s.close()
 if not has_open:
-    print("[-] 1‑100范围内没有发现开放端口")
+    print("[-] 在指定端口范围内没有发现开放端口")
