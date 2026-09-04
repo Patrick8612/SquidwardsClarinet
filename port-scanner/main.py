9689f654e99c181352ae6a343978c5883351a602
Patrick8612 <3259247640@qq.com>
2026-09-04 10:03:08 +0800

    6

--- port-scanner/main.py
+++ port-scanner/main.py
@@ -1,16 +1,19 @@
 import socket
+
 target = input("请输入目标IP：")
-port = int(input("请输入端口："))
+has_open = False
 
-s=socket.socket()
-print("开始连接")
+for port in range(1, 101):
 
-try:
-    s.connect((target,port))
-    print("端口开放")
-except:
-    print("端口关闭")
-finally:
+    s = socket.socket()
+    s.settimeout(0.5)
+    
+    try:
+        s.connect((target, port))
+        print(f"[+] 端口 {port} 开放")
+    except:
+        pass
+
     s.close()
-
-
+if not has_open:
+    print("[-] 1‑100范围内没有发现开放端口")
